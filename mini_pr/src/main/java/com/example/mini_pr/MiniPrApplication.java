ac31b65ea48519a6fd5cddde772f38c3b44c6933
ledung-test <110715254+ledung-test@users.noreply.github.com>
2023-12-30 21:54:42 +0700

    commit

--- mini_pr/src/main/java/com/example/mini_pr/MiniPrApplication.java
+++ mini_pr/src/main/java/com/example/mini_pr/MiniPrApplication.java
@@ -11,12 +11,6 @@ public class MiniPrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiniPrApplication.class, args);
-        Path projectRoot = Paths.get("").toAbsolutePath();
-
-        // Kết hợp với đường dẫn tương đối đến thư mục static
-        Path staticPath = projectRoot.resolve("static");
-
-        System.out.println("Absolute Path to 'static' folder: " + staticPath);
     }
 
 }
