8fe77fbfd383eaf893d18e562f7998ac11d0be20
ledung-test <110715254+ledung-test@users.noreply.github.com>
2024-01-01 19:42:38 +0700

    stream_api

--- stream_api/src/main/java/com/example/stream_api/controller/PersonController.java
+++ stream_api/src/main/java/com/example/stream_api/controller/PersonController.java
@@ -34,21 +34,25 @@ public class PersonController {
         model.addAttribute("sortByCities", personService.getSortedCities());
         return "getSortedCities";
     }
+    //Liệt kê các thành phố và danh sách người đang sống ở thành phố đó
     @GetMapping("/groupPeopleByCity")
     public String groupPeopleByCity(Model model){
         model.addAttribute("groupPeopleByCity", personService.groupPeopleByCity());
         return "groupPeopleByCity";
     }
+    //Liệt kê danh sách nghề nghiệp và số người làm nghề đó
     @GetMapping("/groupJobByCount")
     public String groupJobByCount(Model model){
         model.addAttribute("groupJobByCount", personService.groupJobByCount());
         return "groupJobByCount";
     }
+    //Liệt kê danh sách người có mức lương lớn hơn mức lương trung bình
     @GetMapping("/aboveAverageSalary")
     public String aboveAverageSalary(Model model){
         model.addAttribute("aboveAverageSalary", personService.aboveAverageSalary());
         return "aboveAverageSalary";
     }
+    //Hiển thị người có độ dài tên dài nhất
     @GetMapping("/longestName")
     public String longestName(Model model){
         model.addAttribute("longestName", personService.longestName());
